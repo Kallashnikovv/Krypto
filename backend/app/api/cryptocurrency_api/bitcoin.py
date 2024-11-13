9b7e41c85360910dd10107ff9b36f72135037a22
1wakos <ciapalajulka@gmail.com>
2024-11-14 00:10:26 +0100

    fix: Only chosen cryptocurrencies shown

---
+++ backend/app/api/cryptocurrency_api/bitcoin.py
@@ -0,0 +1,40 @@
+import apikey
+import asyncio
+import aiohttp # imported
+import json
+
+url = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest'
+selected_crypto = 'ADA,AVAX,BNB,BTC,DOGE,ETH,HEX,SOL,USDC,USDT,XRP' # chosen cryptocurrencies
+
+parameters = {
+  'symbol': selected_crypto,
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': apikey.key(),
+}
+
+async def fetch_data(session):
+  async with session.get(url, params=parameters, headers=headers) as response:
+    return await response.json()
+
+
+try:
+  async def get_data():
+    async with aiohttp.ClientSession() as session:
+      data = await fetch_data(session)
+
+    for symbols in data['data']:
+      currency = data['data'][symbols][0]
+      
+      id = currency['id']
+      symbol = currency['symbol']
+      name = currency['name']
+      price = currency['quote']['USD']['price']
+      percentage = currency['quote']['USD']['percent_change_1h']
+
+  asyncio.run(get_data())
+    
+except (aiohttp.ClientConnectionError, aiohttp.ClientTimeout) as e:
+  print(e)

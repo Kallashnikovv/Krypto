b276098f96723b86b415c13af54be093cc2a72fa
Marcin Duda <dudamarcin539@gmail.com>
2024-11-16 16:45:46 +0100

    fix: change fetched data

--- backend/app/models/cryptocurrencies.py
+++ backend/app/models/cryptocurrencies.py
@@ -5,9 +5,7 @@ from pydantic import BaseModel, Field
 #Data Models
 class CryptoRate(BaseModel):
     id: int
-    name: str
     symbol: str
-    price_usd: float
-    percent_change_1h: Optional[float] = None
-    percent_change_24h: Optional[float] = None
-    timestamp: datetime = Field(default_factory=datetime.now)
+    name: str
+    price: float
+    percentage: Optional[float] = None
